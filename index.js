aa3318be2641360746c9951e362abfbc5a4a4876
Kunal Patil <kunalpatil2408.kp@gmail.com>
2021-08-19 20:41:31 +0530

    created basic structure for backend

---
+++ index.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log("Server is running on port", port)
+})
+
