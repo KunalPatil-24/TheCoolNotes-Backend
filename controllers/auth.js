be166ff37df698fd3af947d69332595e5e795f52
Kunal Patil <kunalpatil2408.kp@gmail.com>
2021-08-22 17:54:56 +0530

    completed the signup routre fully

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,7 @@ exports.signUp = (req, res) => {
 
     const { name, email, password } = req.body;
 
-    console.log(`${name} ${email} ${password}`)
+    // console.log(`${name} ${email} ${password}`)
 
 
     //NOTE1:  CHECK IF THE USER IS ALREADY IN THE DATABASE OR NOT
@@ -39,16 +39,16 @@ exports.signUp = (req, res) => {
 
                     client.query(`INSERT INTO users (name,email,password) VALUES ( '${name}' , '${email}', '${hash}');`)
                         .then((data) => {
-                            console.log("************************")
-                            console.log(data);
-                            console.log("************************")
+                            // console.log("************************")
+                            // console.log(data);
+                            // console.log("************************")
                             //NOTE2:  GENERATE TOKEN
                             const token = jwt.sign({
                                 email: email,
 
                             },
                                 process.env.SECRET_KEY
-                            )
+                            );
                             res.status(200).json({
                                 message: "User added successfuly to the database",
                                 token: token
@@ -77,16 +77,51 @@ exports.signUp = (req, res) => {
 exports.signIn = (req, res) => {
 
     const { email, password } = req.body;
-    console.log(` ${email} ${password}`)
+    // console.log(` ${email} ${password}`)
 
-    const isValid = tempDB.findIndex((elem) => elem.email === email);
+    client.query(`SELECT * FROM users WHERE email = '${email}' ;`)
+        .then((data) => {
+            // console.log(data.rows)
+            isValid = data.rows
+            if (isValid.length === 0) {
+                res.send("User does not exist, please sign up first");
+            }
+            else {
+                bcrypt.compare(password, data.rows[0].password, function (err, result) {
+                    if (err) {
+                        res.status(500).json({
+                            error: err,
+                            reason: "server error occured"
+                        })
+                    }
+                    else if (result === true) {
+                        // console.log(result);
+                        // console.log(password);
+                        // console.log(data.rows[0].password);
+                        const token = jwt.sign({
+                            email: email,
 
-    if (isValid === -1) {
-        res.send("User does not exist, please sign up first");
-    }
+                        },
+                            process.env.SECRET_KEY
+                        );
+                        res.status(200).json({
+                            message: "User can sign in successfuly",
+                            token: token
+                        })
+                    }
+                    else {
+                        res.status(400).json({
+                            error: 'Wrong Password',
+                        })
 
-    const hash = tempDB[isValid].password
-    bcrypt.compare(password, hash, function (err, result) {
-        // result == true
-    });
+                    }
+                });
+            }
+        })
+        .catch(err => {
+            res.send(500).json({
+                error: `Database error occured and it is ${err}`,
+            })
+        })
+
 }
